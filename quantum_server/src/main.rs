2ee0853b3a509fc266b05df674445b8c149dbc8b
Yazip <USAASU386@mail.ru>
2025-05-26 18:39:56 +0700

    Created a database, connected it to the server (sqlx), and tested the connection

--- quantum_server/src/main.rs
+++ quantum_server/src/main.rs
@@ -1,10 +1,13 @@
 mod ws;
 mod auth;
+mod db;
 
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
     env_logger::init();
+
+    let pool = db::init_pool().await;
 
     let addr = "127.0.0.1:9001";
     ws::server::run_ws_server(addr).await;
